675f1b9872c6ca43bf88d2b9e4c8296ae18bb601
Luckyluc96 <Lucsaybibi@gmail.com>
2026-04-04 13:45:25 -0400

    Update README.MD. Update build script

--- dockerInit.sh
+++ dockerInit.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/bash
 currentDir=$PWD
 dir="$HOME/.local/share/signal-cli"
+mkdir -p $dir
 echo $dir
+echo directory created if not already existing.
 cd $dir
 git clone https://github.com/bbernhard/signal-cli-rest-api
 cd signal-cli-rest-api
 sudo docker build .
 cd $currentDir
+cp -n config.example.yaml config.yaml
